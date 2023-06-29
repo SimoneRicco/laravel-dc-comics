83283c4f71d97a76867d8b6c85e14c2ef5562618
SimoneRicco <smnrcc0@gmail.com>
2023-06-29 17:13:48 +0200

    bug comics variable

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,6 @@
     </head>
     <body>
         <h1 class="text-center">Ciao da vite</h1>
-        {{-- <img src="{{ Vite::asset('resources/img/picsum30.jpg') }}" alt=""> --}}
+        @dd($comics)
     </body>
 </html>
